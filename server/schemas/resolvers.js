33915e43722659c5c1f04c213fe588300c8d6302
Boba Fett <thomascupchurch@gmail.com>
2021-10-26 11:15:02 -0500

    updates typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,9 @@
+const { User, Thought } = require("../models");
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello World!";
+    thoughts: async () => {
+      return Thought.find().sort({ createdAt: -1 });
     },
   },
 };
