2738d695fcf535193a0eb761b9cb754a78032915
Boba Fett <thomascupchurch@gmail.com>
2021-11-02 10:43:23 -0500

    adds thought form

---
+++ client/src/components/ThoughtForm/index.js
@@ -0,0 +1,21 @@
+import { fromPromise } from "@apollo/client";
+import React, { useState } from "react";
+
+const ThoughtForm = () => {
+  return (
+    <div>
+      <p className="m-0">Character Count: 0/280</p>
+      <form className="flex-row justify-center justify-space-between-md align-stretch">
+        <textarea
+          placeholder="Here's a new thought..."
+          className="form-input col-12 col-md-9"
+        ></textarea>
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ThoughtForm;
